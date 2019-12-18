9e4030c0cfcee9abbae689b9e91fd347939e61e6
Jacob Prudent <jake.prudent@gmail.com>
2019-12-17 19:47:22 -0800

    Met MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
-
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList' ;
+import Movie from "./Movies/Movie";
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
-
+  
+  
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
+    
+    
+
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path ="/" /*compontent= "MovieList"*/>
+        <MovieList/>  
+      </Route>
+      <Route path= "/movies/:id" /*compontent= "Movie"*/>
+        <Movie />  
+      </Route> />
     </div>
   );
 };
